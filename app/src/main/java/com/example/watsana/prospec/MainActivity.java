400adf740c399d6e2d93658e527356546d38a77c
wad <wadwatsana@gmail.com>
2018-11-08 17:44:13 +0700

    8/11/61 17.43

--- app/src/main/java/com/example/watsana/prospec/MainActivity.java
+++ app/src/main/java/com/example/watsana/prospec/MainActivity.java
@@ -28,5 +28,3 @@ public class MainActivity extends AppCompatActivity {
 
     } // Main Method
 } //Main Class
-
-
--- app/src/main/java/com/example/watsana/prospec/bank_ghb/LoginActivity.java
+++ app/src/main/java/com/example/watsana/prospec/bank_ghb/LoginActivity.java
@@ -4,17 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.watsana.prospec.R;
+import com.example.watsana.prospec.Test_RedioButton;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private CardView cardUser,cardUser1,cardUser2,cardPower,cardPower1,cardPower2;
-    private Button button;
+    private CardView cardUser, cardUser1, cardUser2, cardPower, cardPower1, cardPower2,cardSungK,cardSungK1,cardSungK2;
+    private Button button1;
+    private EditText EditTexFai,EditTexSaka,EditTexSoon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,78 +36,96 @@ public class LoginActivity extends AppCompatActivity {
         cardPower2 = (CardView) findViewById(R.id.cardPower2);
 
     }
-        public void checkboxLogin (View view) {
-            boolean checked = ((CheckBox) view).isChecked();
-            switch (view.getId()) {
-                case R.id.checkbox1:
-                    if (!checked)
-                        cardUser.setVisibility(View.GONE);
+
+    public void checkboxLogin(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.checkbox1:
+                if (!checked)
+                    cardUser.setVisibility(View.GONE);
+                else
+                    cardUser.setVisibility(View.VISIBLE);
+                break;
+            case R.id.checkbox2:
+                if (!checked)
+                    cardUser1.setVisibility(View.GONE);
+                else
+                    cardUser1.setVisibility(View.VISIBLE);
+                break;
+            case R.id.checkbox3:
+                if (!checked)
+                    cardUser2.setVisibility(View.GONE);
+                else
+                    cardUser2.setVisibility(View.VISIBLE);
+                break;
+
+            case R.id.checkbox4:
+                if (!checked)
+                    cardPower.setVisibility(View.GONE);
+                else
+                    cardPower.setVisibility(View.VISIBLE);
+                break;
+            case R.id.checkbox5:
+                if (!checked)
+                    cardPower1.setVisibility(View.GONE);
+                else
+                    cardPower1.setVisibility(View.VISIBLE);
+                break;
+            case R.id.checkbox6:
+                if (!checked)
+                    cardPower2.setVisibility(View.GONE);
+                else
+                    cardPower2.setVisibility(View.VISIBLE);
+                break;
+        }
+
+        EditTexFai = (EditText) findViewById(R.id.EditTexFai);
+        EditTexSaka = (EditText) findViewById(R.id.EditTexSaka);
+        EditTexSoon = (EditText) findViewById(R.id.EditTexSoon);
+
+        cardSungK = (CardView) findViewById(R.id.cardSungK);
+        cardSungK1 = (CardView) findViewById(R.id.cardSungK1);
+        cardSungK2 = (CardView) findViewById(R.id.cardSungK2);
+    }
+
+        public void redioLogin (View view) {
+            boolean checked = ((RadioButton) view).isChecked();
+            switch(view.getId()) {
+                case R.id.RadioButton1:
+                    if (checked)
+                        EditTexFai.setVisibility(View.GONE);
                     else
-                        cardUser.setVisibility(View.VISIBLE);
-                    break;
-                case R.id.checkbox2:
-                    if (!checked)
-                        cardUser1.setVisibility(View.GONE);
-                    else
-                        cardUser1.setVisibility(View.VISIBLE);
-                    break;
-                case R.id.checkbox3:
-                    if (!checked)
-                        cardUser2.setVisibility(View.GONE);
-                    else
-                        cardUser2.setVisibility(View.VISIBLE);
+                         EditTexFai.setVisibility(View.VISIBLE);
                     break;
 
-                case R.id.checkbox4:
-                    if (!checked)
-                        cardPower.setVisibility(View.GONE);
+                case R.id.RadioButton2:
+                    if (checked)
+                        EditTexSaka.setVisibility(View.GONE);
                     else
-                        cardPower.setVisibility(View.VISIBLE);
+                        EditTexSaka.setVisibility(View.VISIBLE);
                     break;
-                case R.id.checkbox5:
-                    if (!checked)
-                        cardPower1.setVisibility(View.GONE);
+
+                case R.id.RadioButton3:
+                    if (checked)
+                        EditTexSoon.setVisibility(View.GONE);
                     else
-                        cardPower1.setVisibility(View.VISIBLE);
+                        EditTexSoon.setVisibility(View.VISIBLE);
                     break;
-                case R.id.checkbox6:
-                    if (!checked)
-                        cardPower2.setVisibility(View.GONE);
-                    else
-                        cardPower2.setVisibility(View.VISIBLE);
-                    break;
+
+
             }
-
-//
-//            public void redioLogin(View view) {
-//                boolean checked = ((RadioButton) view).isChecked();
-//                switch(view.getId()) {
-//                    case R.id.RadioButton1:
-//                        if (checked)
-//
-//                        break;
-//                    case R.id.RadioButton2:
-//                        if (checked)
-//
-//                        break;
-//                    case R.id.RadioButton3:
-//                        if (checked)
-//
-//                        break;
-//                }
-//            }
 
 //      Bind Widget
-        button = (Button) findViewById(R.id.button);
+                button1 = (Button) findViewById(R.id.button1);
 
 //      Get Event From Click Menu Bank
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, MenuBanksActivity.class));
-            }
-        });
+                button1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(LoginActivity.this, MenuBanksActivity.class));
+                    }
+                });
 
 
-    } // Main Method
-} //Main Class
+        } // Main Method
+    } //Main Class
